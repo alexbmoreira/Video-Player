f2581446a46d798611a0171f6bc73821eb6e2b82
Alex Moreira <alexbmoreira@gmail.com>
2020-05-16 20:17:14 -0400

    Video player controls fully functional

--- index.js
+++ index.js
@@ -41,15 +41,39 @@ function updateIcon(paused)
 
 function stopVideo()
 {
-
+    video.currentTime = 0;
+    video.pause();
+    updateIcon(true);
 }
 
 function updateProgress()
 {
-    return true;
+    progress.value = (video.currentTime / video.duration) * 100;
+
+    timestamp.innerText = convertTime(video.currentTime);
+}
+
+function convertTime(time)
+{
+    var min = 0;
+    var sec = 0;
+
+    min = Math.floor(time / 60);
+    if (min < 10)
+    {
+        min = "0" + String(min);
+    }
+
+    sec = Math.floor(time % 60);
+    if (sec < 10)
+    {
+        sec = "0" + String(sec);
+    }
+
+    return min + ":" + sec;
 }
 
 function setVideoProgress()
 {
-    return true;
+    video.currentTime = +progress.value / 100 * video.duration;
 }
